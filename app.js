e22a0c27339a5b544fd66ae747a68b267a18cf9d
Gary Wilson <54655467+gwil05@users.noreply.github.com>
2020-05-08 11:34:33 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// const toggleModal = () => {
+//     document.querySelector(".modal").classList.toggle('hidden');
+// };
+
+// document.querySelector('#show').addEventListener('click', toggleModal );
+
+// document.querySelector('#learn-more').addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     toggleModal();
+// });
+
+
+const toggleModal = () => {
+    document.querySelector('.modal')
+      .classList.toggle('modal--hidden');
+    document.querySelector('.overlay')
+      .classList.toggle('overlay--hidden');
+  }
+  document.querySelector('#show-modal')
+    .addEventListener('click', toggleModal);
+  
+  document.querySelector('.overlay')
+    .addEventListener('click', toggleModal);
+  
+  document.querySelector('#learn-more-form')
+    .addEventListener('submit', (e) => {
+    e.preventDefault();
+    toggleModal();
+  });
+  
+  document.querySelector('.modal-close span')
+    .addEventListener('click', toggleModal);
+  document.querySelector('.overlay')
+    .addEventListener('click', toggleModal);
